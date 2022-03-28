736ba04b5d8a45019ddd11821974ad8f56d87db8
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-03-28 15:05:24 -0600

    Feat(page): add pass recovery page

---
+++ src/pages/auth/passwordRecovery.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PassRecoveryForm from "../../features/auth/components/PassRecoveryForm/PassRecoveryForm"
+
+const PasswordRecovery = () => {
+  return (
+    <>
+      <PassRecoveryForm />
+    </>
+  )
+}
+
+export default PasswordRecovery
