89cdea998124bb5a2670cf6a79e74264ddabb57e
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-03-30 17:11:03 -0600

    Feat: get logged in user after app restart

--- src/shared/components/Layout/layout.js
+++ src/shared/components/Layout/layout.js
@@ -1,11 +1,34 @@
-import React from "react"
+import React, { useContext, useEffect } from "react"
+import agent from "../../../API/agent"
 import NavBar from "../Navbar/Navbar"
 import Footer from "../Footer/Footer"
 import { ToastContainer, Zoom } from "react-toastify"
 import * as styles from "../Layout/layout.module.css"
-// import Crisp from "./Crisp/Crisp"
+import { UserContext } from "../../../context/UserContext"
+// import Crisp from "../../../features/chat/components/Crisp/Crisp"
 
 const Layout = ({ children }) => {
+  const {setUser, setIsloggedIn} = useContext(UserContext)
+
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      const token = window.localStorage.getItem("bargasJwt")
+      if (!token) return
+      try {
+        const user = await agent.user.getLoggedInUser({
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        setUser(user)
+        setIsloggedIn(true)
+      } catch (err) {
+        window.localStorage.removeItem("bargasJwt")
+      }
+    }
+    getLoggedInUser()
+  },[setUser])
+
   return (
     <>
       <div className={styles.fixedBg}></div>
