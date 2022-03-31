848003064d9edf3ddd3270e0bfbf1ae8536732d5
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-03-30 21:19:42 -0600

    Refactor: del unused code + cleanup at auth dp

--- src/shared/components/Navbar/AuthDropdown/AuthDropdown.js
+++ src/shared/components/Navbar/AuthDropdown/AuthDropdown.js
@@ -1,25 +1,17 @@
-import { Link, navigate } from "gatsby"
 import React, { useContext } from "react"
+import { UserContext } from "../../../../context/UserContext"
 import { Dropdown } from "react-bootstrap"
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 import { BiUser } from "react-icons/bi"
-import { UserContext } from "../../../../context/UserContext"
+import { Link } from "gatsby"
 import * as styles from "./authDropdown.module.css"
+
 
 const AuthDropdown = () => {
   const { isLoggedIn } = useContext(UserContext)
 
-  console.log(isLoggedIn)
-  const handleLinkKey = key => {
-    if (key === "login") {
-      navigate("/auth/login")
-    } else {
-      navigate("/auth/signup")
-    }
-  }
-
   return (
-    <Dropdown onSelect={handleLinkKey} as={ButtonGroup}>
+    <Dropdown as={ButtonGroup}>
       <Dropdown.Toggle className={styles.authDp} id="dropdown-custom-1">
         <BiUser className={styles.icon} />
       </Dropdown.Toggle>
