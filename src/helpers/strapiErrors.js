bfa440ea8be1c4375e7d1ac3b15441f5937857f0
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-04-04 20:21:19 -0600

    Feat: add helper for possible Strapi API errors

---
+++ src/helpers/strapiErrors.js
@@ -0,0 +1,22 @@
+export const StrapiErrors = {
+  LOGIN: [
+    {
+      id: "Auth.form.error.invalid",
+      message: "Email o contraseña incorrecto.",
+    },
+    {
+      id: "Auth.form.error.confirmed",
+      message: "El email de tu cuenta no ha sido confirmado.",
+    },
+    {
+      id: "Auth.form.error.blocked",
+      message: "Tu cuenta ha sido bloqueada.",
+    },
+  ],
+  SIGNUP: [
+    {
+      id: "Auth.form.error.email.taken",
+      message: "El email ya ha sido utilizado.",
+    },
+  ],
+}
