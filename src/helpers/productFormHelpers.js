629fe9c75ecc6dc3a122d95b89633bdcd827b24d
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-04-13 23:51:50 -0600

    Feat(component): add product form (formik)

--- src/helpers/productFormHelpers.js
+++ src/helpers/productFormHelpers.js
@@ -1,16 +1,85 @@
-import { v4 as uuid } from 'uuid';
-
-export function cartItemBuilder(strapiProductData, formData) {
-    let cartItem = {
-        id: strapiProductData.strapi_id,
-        inCartId: uuid(), 
-        name: strapiProductData.name,
-        price: strapiProductData.price,
-        mains: strapiProductData.mains,
-        sides: strapiProductData.sides,
-        thumbnail: `${strapiProductData.image.formats.thumbnail.url}`,
-        qualityImg: strapiProductData.image.localFile.childImageSharp.gatsbyImageData,
-        ...formData 
+const getInitialValues = (productData, isEditMode) => {
+  if (isEditMode) {
+    const initialValues = {
+      quantity: productData.quantity,
+      main: productData.main,
+      side: productData.side,
+      sauce: productData.sauce,
+      size: productData.size,
+      extraInfo: productData.extraInfo,
+      subTotal: productData.subTotal,
     }
-    return cartItem
+    return initialValues
+  }
+
+  const { sides, mains, sauces, sizes } = productData
+  const initialValues = {
+    quantity: 1,
+    main: mains.length > 0 ? mains[0].name : "",
+    side: sides.length > 0 ? sides[0].name : "",
+    sauce: sauces.length > 0 ? sauces[0].name : "",
+    size: sizes.length > 0 ? sizes[0].name : "",
+    extraInfo: "",
+    subTotal: productData.price,
+  }
+  return initialValues
+}
+
+const getRadioGroupInfo = (index, context) => {
+  let groupInfo = ""
+  switch (context) {
+    case "getName":
+      if (index === 0) groupInfo = "side"
+      if (index === 1) groupInfo = "main"
+      if (index === 2) groupInfo = "sauce"
+      if (index === 3) groupInfo = "size"
+      break
+    case "getLabel":
+      if (index === 0) groupInfo = "Acompañamiento"
+      if (index === 1) groupInfo = "Ingrediente principal"
+      if (index === 2) groupInfo = "Salsa"
+      if (index === 3) groupInfo = "Tamaño"
+      break
+    default:
+      break
+  }
+  return groupInfo
+}
+
+const isCheckedByDefault = (isEditMode, productData, radioItemInfo) => {
+  if (isEditMode) {
+    const selectedOptions = [
+      productData.side,
+      productData.main,
+      productData.sauce,
+      productData.size,
+    ]
+    for (let i = 0; i < selectedOptions.length; i++) {
+      if (selectedOptions[i] === radioItemInfo.itemName) {
+        return true
+      }
+    }
+  } else {
+    if (radioItemInfo.itemIdx === 0) return true
+  }
+  return false
+}
+
+const getSubTotal = (productData, formValues) => {
+  let productPrice = productData.price
+  if (formValues.size !== "") {
+    const selectedSizePrice = productData.sizes.find(
+      x => x.name === formValues.size
+    ).price
+    if (selectedSizePrice) productPrice = selectedSizePrice
+  }
+  productPrice = productPrice - (productPrice * productData.discount) / 100
+  return productPrice * formValues.quantity
+}
+
+export const helper = {
+  getInitialValues,
+  getRadioGroupInfo,
+  isCheckedByDefault,
+  getSubTotal,
 }
---
+++ src/shared/components/Form/ProductForm/ProductForm.js
@@ -0,0 +1,101 @@
+import React, { useContext } from "react"
+import { CartContext } from "../../../../context/CartContext"
+import { Form, Formik } from "formik"
+import { helper } from "../../../../helpers/productFormHelpers"
+import BargasQuantityInput from "../BargasQuantityInput/BargasQuantityInput"
+import RadioGroupWrapper from "../RadioGroupWrapper/RadioGroupWrapper"
+import BargasRadioButton from "../BargasRadioButton/BargasRadioButton"
+import BargasTextAreaField from "../BargasTextAreaField/BargasTextAreaField"
+import { Button } from "react-bootstrap"
+import * as styles from "./productForm.module.css"
+
+const ProductForm = ({
+  productData,
+  isEditMode,
+  handleCloseEditModal,
+}) => {
+    const {addToCart, editCartItem} = useContext(CartContext)
+    const productOptions = [
+      productData.sides,
+      productData.mains,
+      productData.sauces,
+      productData.sizes,
+    ]
+  return (
+    <Formik
+      initialValues={helper.getInitialValues(productData, isEditMode)}
+      onSubmit={(values, formikBag) => {
+        values.subTotal = helper.getSubTotal(productData, values)
+        isEditMode
+          ? editCartItem(productData, values)
+          : addToCart(productData, values)
+      }}
+    >
+      {formik => (
+        <Form onSubmit={formik.handleSubmit}>
+          <BargasQuantityInput label="Cantidad" type="number" name="quantity" />
+          {productOptions.map(
+            (optionsGroup, groupIndex) =>
+              optionsGroup.length > 0 && (
+                <RadioGroupWrapper
+                  key={groupIndex}
+                  label={helper.getRadioGroupInfo(groupIndex, "getLabel")}
+                >
+                  {optionsGroup.map((item, groupItemIndex) => (
+                    <BargasRadioButton
+                      key={item.id}
+                      id={item.name}
+                      type="radio"
+                      name={helper.getRadioGroupInfo(groupIndex, "getName")}
+                      label={item.name}
+                      value={item.name}
+                      defaultChecked={helper.isCheckedByDefault(
+                        isEditMode,
+                        productData,
+                        { itemName: item.name, itemIdx: groupItemIndex }
+                      )}
+                    />
+                  ))}
+                </RadioGroupWrapper>
+              )
+          )}
+          <BargasTextAreaField
+            label="Solicitud especial"
+            name="extraInfo"
+            placeholder="Escribe en este espacio si deseas eliminar alguno de los ingredientes mostrados"
+            rows="5"
+          />
+          <span className={styles.subTotal}>
+            Subtotal: ₡{" "}
+            {new Intl.NumberFormat("CRC").format(
+              helper.getSubTotal(productData, formik.values)
+            )}
+          </span>
+          {!isEditMode ? (
+            <button className={styles.submitBtn}>Ordenar</button>
+          ) : (
+            <div className={styles.actions}>
+              <Button variant="success" type="submit">
+                Actualizar
+              </Button>
+              <Button
+                variant="danger"
+                onClick={handleCloseEditModal}
+              >
+                Cerrar
+              </Button>
+            </div>
+          )}
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+ProductForm.defaultProps = {
+  productData: {},
+  isEditMode: false,
+  handleCloseEditModal: () => {},
+}
+
+export default ProductForm
