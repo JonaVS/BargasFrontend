b94dade57455afe6e5849ac1bf9a29a11b8fa227
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-04-13 23:36:26 -0600

    Feat: move core cart actions to CartContext

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,32 +1,74 @@
 import React, { createContext } from "react"
 import { useLocalStorage } from "../customHooks/useLocalStorage"
+import { v4 as uuid } from "uuid"
 
 export const CartContext = createContext([{}])
 
 export const CartProvider = ({ children }) => {
-
   const [cart, setCart] = useLocalStorage("cart", [])
 
   const getCartTotal = () => {
     let subTotal = 0
-    // let delivery = 2000
-    // let discount = 0.5
-
     //The total for products only
     cart.forEach(item => (subTotal += parseInt(item.price) * item.quantity))
-
-    //This applies the delivery cost
-    // subTotal += delivery
-
-    //Set the cartTotal state
     return subTotal
+  }
+
+  const addToCart = (productData, formData) => {
+    const cartItem = {
+      id: productData.strapi_id,
+      inCartId: uuid(),
+      name: productData.name,
+      price: productData.price,
+      discount: productData.discount,
+      mains: productData.mains,
+      sides: productData.sides,
+      sauces: productData.sauces,
+      sizes: productData.sizes,
+      thumbnail: productData.image.formats.thumbnail.url,
+      qualityImg: productData.image.localFile.childImageSharp.gatsbyImageData,
+      ...formData,
+    }
+    setCart([...cart, cartItem])
+  }
+
+  const editCartItem = (productData, formData) => {
+    let updatedCart = cart.map(item => {
+      if (item.inCartId === productData.inCartId) {
+        return {
+          ...item,
+          quantity: formData.quantity,
+          side: formData.side,
+          main: formData.main,
+          sauce: formData.sauce,
+          size: formData.size,
+          extraInfo: formData.extraInfo,
+          subTotal: formData.subTotal,
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  const deleteCartItem = inCartId => {
+    let newCart = cart.filter(item => item.inCartId !== inCartId)
+    setCart(newCart)
   }
 
   const cartTotal = getCartTotal()
 
-
   return (
-    <CartContext.Provider value={{cart, setCart, cartTotal}}>
+    <CartContext.Provider
+      value={{
+        cart,
+        setCart,
+        cartTotal,
+        addToCart,
+        editCartItem,
+        deleteCartItem,
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
