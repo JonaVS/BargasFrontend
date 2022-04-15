48c4abd999d64943f0ebb031560a565213df11da
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-04-14 19:28:40 -0600

    Fix: destructure prop to prevent browser error at textarea

--- src/shared/components/Form/BargasTextAreaField/BargasTextAreaField.js
+++ src/shared/components/Form/BargasTextAreaField/BargasTextAreaField.js
@@ -2,12 +2,12 @@ import React from "react"
 import { useField } from "formik"
 import * as styles from "../formFields.module.css"
 
-const BargasTextAreaField = ({ label, ...props }) => {
+const BargasTextAreaField = ({ label, wrapperClass, ...props }) => {
   const [field, meta] = useField(props)
 
   return (
     <>
-      <div className={`${styles.inputWrapper} ${props.wrapperClass}`}>
+      <div className={`${styles.inputWrapper} ${wrapperClass}`}>
         <label className={styles.formLabel} htmlFor={props.id || props.name}>
           {label}
         </label>
