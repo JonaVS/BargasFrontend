5edc0a504c6b04d7adafb1f7d72275820a8fa6f7
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-04-16 00:40:13 -0600

    Feat: cart page is now client only

--- src/pages/app/[...].js
+++ src/pages/app/[...].js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Router } from "@reach/router"
+import CartPage from "../../features/cart/pages/CartPage"
 
 /*This special page handles client only routes (Gatsby will not 
 generate static versions for the pages/components inside the router)
@@ -9,7 +10,7 @@ For example: login, signup, cart, profile, and others will go here.
 const App = () => {
   return (
     <Router>
-
+      <CartPage path="app/cart"/>
     </Router>
   )
 }
