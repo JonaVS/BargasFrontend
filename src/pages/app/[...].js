96d29e467df8bf2bd2defceb84fde2e7c0c06d72
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-04-16 00:23:28 -0600

    Feat: add page that handles client only routes

---
+++ src/pages/app/[...].js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Router } from "@reach/router"
+
+/*This special page handles client only routes (Gatsby will not 
+generate static versions for the pages/components inside the router)
+
+For example: login, signup, cart, profile, and others will go here.
+*/
+const App = () => {
+  return (
+    <Router>
+
+    </Router>
+  )
+}
+
+export default App
