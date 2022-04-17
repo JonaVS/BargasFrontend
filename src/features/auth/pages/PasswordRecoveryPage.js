5082c4f76f7cafc51d1ed467a270107cf6a7346b
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-04-16 19:24:40 -0600

    Refactor: move auth related pages to new folder

---
+++ src/features/auth/pages/PasswordRecoveryPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PassRecoveryForm from "../components/PassRecoveryForm/PassRecoveryForm"
+
+const PasswordRecoveryPage = () => {
+  return (
+    <>
+      <PassRecoveryForm />
+    </>
+  )
+}
+
+export default PasswordRecoveryPage
---
+++ src/features/auth/pages/SignUpPage.js
@@ -0,0 +1,8 @@
+import React from "react"
+import RegisterForm from "../components/RegisterForm/RegisterForm"
+
+const SignUpPage = () => {
+  return <RegisterForm />
+}
+
+export default SignUpPage
