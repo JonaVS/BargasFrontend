29438f16dda7ac6e46b8a92860d86e3e943e2f78
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-04-16 19:26:49 -0600

    Feat: set login, signup, passRecovery as client only pages

--- src/pages/app/[...].js
+++ src/pages/app/[...].js
@@ -2,6 +2,9 @@ import React from "react"
 import { Router } from "@reach/router"
 import NotFoundPage from "../404"
 import CartPage from "../../features/cart/pages/CartPage"
+import SignUpPage from "../../features/auth/pages/SignUpPage"
+import LoginPage from "../../features/auth/pages/LoginPage"
+import PasswordRecoveryPage from "../../features/auth/pages/PasswordRecoveryPage"
 
 /*This special page handles client only routes (Gatsby will not 
 generate static versions for the pages/components inside the router)
@@ -12,6 +15,9 @@ const App = () => {
   return (
     <Router>
       <CartPage path="app/cart"/>
+      <SignUpPage path="app/signup"/>
+      <LoginPage path="app/login"/>
+      <PasswordRecoveryPage path="app/forgot-password"/>
       <NotFoundPage default/>
     </Router>
   )
