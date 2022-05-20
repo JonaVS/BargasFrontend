95c054af26d84f8b447486934c811b265d490741
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-05-20 16:16:15 -0600

    Feat: use ConfirmationModal at CartActions

--- src/features/cart/components/CartActions/CartActions.js
+++ src/features/cart/components/CartActions/CartActions.js
@@ -1,21 +1,28 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { CartContext } from "../../../../context/CartContext"
 import Dropdown from "react-bootstrap/Dropdown"
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 import { BsFilterRight, BsTrash } from "react-icons/bs"
 import { SortBy } from "../../../../helpers/cartSort"
 import { Button } from "react-bootstrap"
+import ConfirmationModal from "../../../../shared/components/ConfirmationModal/ConfirmationModal"
 import * as styles from "../CartActions/cartActions.module.css"
 
 const CartActions = () => {
   const { sortCart, setCart } = useContext(CartContext)
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
 
   const handleFilterKey = (key) => {
      sortCart(key)
   }
 
   const handleDelete = () => {
+    setShowConfirmModal(false)
     setCart([])
+  }
+
+  const handleConfirmModal = () => {
+    setShowConfirmModal(!showConfirmModal)
   }
 
   return (
@@ -23,7 +30,7 @@ const CartActions = () => {
       <Dropdown onSelect={handleFilterKey} as={ButtonGroup}>
         <Dropdown.Toggle className={styles.sortDp}>
           Ordenar
-          <BsFilterRight className={styles.icon}/>
+          <BsFilterRight className={styles.icon} />
         </Dropdown.Toggle>
         <Dropdown.Menu className={styles.dpMenu}>
           <Dropdown.Item eventKey={SortBy.SUBTOTAL}>Subtotal</Dropdown.Item>
@@ -31,9 +38,21 @@ const CartActions = () => {
           <Dropdown.Item eventKey={SortBy.QUANTITY}>Cantidad</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
-      <Button variant="custom" className={styles.emptyCartBtn} onClick={handleDelete}>
+      <Button
+        variant="custom"
+        className={styles.emptyCartBtn}
+        onClick={handleConfirmModal}
+      >
         Eliminar todo <BsTrash />
       </Button>
+      <ConfirmationModal
+        show={showConfirmModal}
+        close={handleConfirmModal}
+        action={handleDelete}
+        contextTitle="Eliminar items"
+        actionText="Eliminar"
+        message="¿Deseas eliminar todos los items?"
+      />
     </div>
   )
 }
