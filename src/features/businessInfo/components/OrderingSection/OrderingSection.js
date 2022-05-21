af1e6180f277b1ddd3cc594e29d869ce2bb3b033
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-05-21 00:31:14 -0600

    Feat: get online ordering HomePage info via query

--- src/features/businessInfo/components/OrderingSection/OrderingSection.js
+++ src/features/businessInfo/components/OrderingSection/OrderingSection.js
@@ -1,18 +1,38 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import LazyLoad from "react-lazyload"
 import Container from "react-bootstrap/Container"
 import SubSectionHeader from "../../../../shared/components/SubSectionHeader/SubSectionHeader"
 import OrderingCols from "./OrderingCols/OrderingCols"
 
 const OrderingSection = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      strapiHomePage {
+        aboutOrdering {
+          description
+        }
+        aboutOrderingCols {
+          onlineOrdering
+          delivery
+          chatHelp
+        }
+      }
+    }
+  `)
+
+  const sectionDesc = data.strapiHomePage.aboutOrdering.description
+
   return (
     <LazyLoad once offset={50} height={500}>
       <Container fluid="xl">
-        <SubSectionHeader title="Pedidos en linea" withLink link="/menu" linkText="Ordenar">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-          ea commodo consequat.
+        <SubSectionHeader
+          title="Pedidos en linea"
+          withLink
+          link="/menu"
+          linkText="Ordenar"
+        >
+          {sectionDesc}
         </SubSectionHeader>
         <OrderingCols />
       </Container>
