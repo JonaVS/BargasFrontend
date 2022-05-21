b1c9f0202068327caef086bf776dc40ea19def78
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-05-21 00:03:06 -0600

    Feat: get about us HomePage info via query

--- src/features/businessInfo/components/AboutSection/AboutSection.js
+++ src/features/businessInfo/components/AboutSection/AboutSection.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import LazyLoad from "react-lazyload"
 import Container from "react-bootstrap/Container"
 import SubSectionHeader from "../../../../shared/components/SubSectionHeader/SubSectionHeader"
@@ -7,20 +8,35 @@ import AboutCols from "./AboutCols/AboutCols"
 import * as styles from "./aboutSection.module.css"
 
 const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      strapiHomePage {
+        aboutImage {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, quality: 60, width: 650)
+            }
+          }
+        }
+        aboutUs {
+          description
+        }
+      }
+    }
+  `)
+
+  const sectionDesc = data.strapiHomePage.aboutUs.description
+
   return (
     <LazyLoad once offset={100} height={400}>
       <Container fluid="xl">
         <div className={styles.content}>
           <div className={styles.aboutInfo}>
-            <SubSectionHeader title="Sobre nosotros" className={styles.aboutContainer}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt
-              ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.
+            <SubSectionHeader
+              title="Sobre nosotros"
+              className={styles.aboutContainer}
+            >
+            {sectionDesc}
             </SubSectionHeader>
           </div>
           <StaticImage
