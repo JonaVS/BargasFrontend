948997faef0e35cf6278e6c14cd7a6c6fab8869f
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-05-21 00:46:25 -0600

    Refactor: remove string interpolations at OrderingCols

--- src/features/businessInfo/components/OrderingSection/OrderingCols/OrderingCols.js
+++ src/features/businessInfo/components/OrderingSection/OrderingCols/OrderingCols.js
@@ -10,19 +10,19 @@ const OrderingCols = ({ colData }) => {
   return (
     <Container className={styles.mainCont}>
       <Row className="justify-content-center">
-        <Col  lg={true} className={`${styles.col}`}>
+        <Col  lg={true} className={styles.col}>
           <FaClipboardList className={styles.icon}/>
           <p>
             {colData.onlineOrdering}
           </p>
         </Col>
-        <Col lg={true} className={`${styles.col}`}>
+        <Col lg={true} className={styles.col}>
           <FaMotorcycle className={styles.icon}/>
           <p>
             {colData.delivery}
           </p>
         </Col>
-        <Col lg={true} className={`${styles.col}`}>
+        <Col lg={true} className={styles.col}>
           <BsFillChatDotsFill className={styles.icon}/>
           <p>
             {colData.chatHelp}
