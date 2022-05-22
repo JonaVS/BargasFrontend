485c4bc8af01c136c2c29a89226cf2c96a073230
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-05-21 22:38:18 -0600

    Feat: get terms and conditions page info via query

--- src/pages/info/termsAndConditions.js
+++ src/pages/info/termsAndConditions.js
@@ -1,9 +1,35 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Seo from "../../shared/components/Seo/seo"
 import Info from "../../templates/Info"
 
+const TermsAndConditions = ({ data }) => {
 
-const TermsAndConditions = () => {
-  return <Info title='Terminos y Condiciones'/>
+  const pageSeo = data.strapiTermsAndConditionsPage.seoData
+  const pageMarkdown = data.strapiTermsAndConditionsPage.markdown.data.markdown
+
+  return (
+    <>
+      <Seo title={pageSeo.title} description={pageSeo.description}/>
+      <Info markdown={pageMarkdown} />
+    </>
+  )
 }
 
 export default TermsAndConditions
+
+export const termsQuery = graphql`
+  query TermsPageData {
+    strapiTermsAndConditionsPage {
+      seoData {
+        title
+        description
+      }
+      markdown {
+        data {
+          markdown
+        }
+      }
+    }
+  }
+`
