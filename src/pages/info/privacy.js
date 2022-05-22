f0edc920cc36a276c9c6342dd38705e678363f90
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-05-21 22:56:35 -0600

    Feat: get Privacy page info via query

--- src/pages/info/privacy.js
+++ src/pages/info/privacy.js
@@ -1,9 +1,35 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Seo from "../../shared/components/Seo/seo"
 import Info from "../../templates/Info"
 
+const Privacy = ({ data }) => {
 
-const Privacy = () => {
-  return <Info title='Politica de Privacidad'/>
+  const seoData = data.strapiPrivacyPage.seoData
+  const pageMarkdown = data.strapiPrivacyPage.markdown.data.markdown
+
+  return (
+    <>
+      <Seo title={seoData.title} description={seoData.description} />
+      <Info markdown={pageMarkdown} />
+    </>
+  )
 }
 
 export default Privacy
+
+export const privacyQuery = graphql`
+  query privacyPageData {
+    strapiPrivacyPage {
+      seoData {
+        title
+        description
+      }
+      markdown {
+        data {
+          markdown
+        }
+      }
+    }
+  }
+`
