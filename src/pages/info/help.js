69a3e00892a97202162ed0ff959ca2d8a5632caf
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-05-21 22:57:12 -0600

    Feat: get help page info via query

--- src/pages/info/help.js
+++ src/pages/info/help.js
@@ -1,9 +1,35 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Seo from "../../shared/components/Seo/seo"
 import Info from "../../templates/Info"
 
+const Help = ({ data }) => {
 
-const Help = () => {
-  return <Info title='Ayuda de compra'/>
+  const seoData = data.strapiOrderingHelpPage.seoData
+  const pageMarkdown = data.strapiOrderingHelpPage.markdown.data.markdown
+
+  return (
+    <>
+      <Seo title={seoData.title} description={seoData.description} />
+      <Info markdown={pageMarkdown} />
+    </>
+  )
 }
 
 export default Help
+
+export const helpQuery = graphql`
+  query helpPageData {
+    strapiOrderingHelpPage {
+      seoData {
+        title
+        description
+      }
+      markdown {
+        data {
+          markdown
+        }
+      }
+    }
+  }
+`
