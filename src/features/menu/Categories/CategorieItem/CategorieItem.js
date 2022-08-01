e84b3a4b8693d634eb0b81fab71459a883a06936
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-07-31 20:54:26 -0600

    Refactor: Use normal btn tag at CategorieItem

--- src/features/menu/Categories/CategorieItem/CategorieItem.js
+++ src/features/menu/Categories/CategorieItem/CategorieItem.js
@@ -5,16 +5,15 @@ import * as styles from "../CategorieItem/categorieItem.module.css"
 const CategorieItem = ({ name, handleCategorie, currentCategorie }) => {
   return (
     <li className={styles.listItem}>
-      <Button
+      <button
         value={name}
         onClick={e => handleCategorie(e)}
-        variant="custom"
         className={`${
           currentCategorie === name ? styles.selectedCat : styles.catBtn
         }`}
       >
         {name}
-      </Button>
+      </button>
     </li>
   )
 }
