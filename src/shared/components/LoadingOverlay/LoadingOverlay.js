1411f0d207d7befa96c66d38870719217627fa12
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-09-28 16:11:02 -0600

    Feat: Add text to LoadingOverlay component

--- src/shared/components/LoadingOverlay/LoadingOverlay.js
+++ src/shared/components/LoadingOverlay/LoadingOverlay.js
@@ -2,17 +2,22 @@ import React from "react"
 import { Spinner } from "react-bootstrap"
 import * as styles from "./loadingOverlay.module.css"
 
-const LoadingOverlay = ({ className }) => {
+const LoadingOverlay = ({ className, msgContClass, message }) => {
   return (
     <div className={`${styles.loadingOverlay} ${className}`}>
-      <Spinner animation="border" role="status" variant="warning">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
+      <Spinner animation="border" role="status" variant="warning" />
+      {message && (
+        <div className={`${styles.msgContainer} ${msgContClass}`}>
+          <span>{message}</span>
+        </div>
+      )}
     </div>
   )
 }
 LoadingOverlay.defaultProps = {
   className: "",
+  msgContClass: "",
+  message: null,
 }
 
 export default LoadingOverlay
