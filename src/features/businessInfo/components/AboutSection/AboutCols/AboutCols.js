b453c85d1a1e9f287ee608e4e93389a5767bba3b
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-11-15 18:18:14 -0600

    Feat: Home about section animations

--- src/features/businessInfo/components/AboutSection/AboutCols/AboutCols.js
+++ src/features/businessInfo/components/AboutSection/AboutCols/AboutCols.js
@@ -1,27 +1,70 @@
 import React from "react"
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import { Row, Col } from "react-bootstrap"
+import { motion } from "framer-motion"
 import { MdLocationOn } from "react-icons/md"
 import { BiCurrentLocation } from "react-icons/bi"
 import { AiOutlineClockCircle } from "react-icons/ai"
 import * as styles from "./aboutCols.module.css"
 
+//START--Framer motion variants--START
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.8,
+      staggerChildren: 0.5
+    }
+  }
+}
+
+const item = {
+  hidden: { opacity: 0, y: 150 },
+  show: { opacity: 1, y: 0, transition: {duration: 1} }
+}
+//END--Framer motion variants--END
+
 const AboutCols = ({colsData}) => {
   const {basicLocation, detailedLocation, openingHours  } = colsData
-  
+
   return (
     <Container className={styles.mainCont} fluid>
-      <Row className={`justify-content-center ${styles.row}`}>
-        <Col md={4} lg={true} className={`${styles.col}`}>
+      <Row
+        as={motion.div}
+        className={`justify-content-center ${styles.row}`}
+        variants={container}
+        initial='hidden'
+        whileInView="show"
+        viewport={{ fallback: true, once: true }} 
+      >
+        <Col
+          as={motion.div}
+          md={4}
+          lg={true}
+          className={`${styles.col}`}
+          variants={item}
+        >
           <MdLocationOn className={styles.icon} />
           <p>{basicLocation}</p>
         </Col>
-        <Col md={4} lg={true} className={`${styles.col}`}>
+        <Col
+          as={motion.div}
+          md={4}
+          lg={true}
+          className={`${styles.col}`}
+          variants={item}
+        >
           <BiCurrentLocation className={styles.icon} />
           <p>{detailedLocation}</p>
         </Col>
-        <Col md={4}  lg={true} className={`${styles.col}`}>
+        <Col
+          as={motion.div}
+          md={4}
+          lg={true}
+          className={`${styles.col}`}
+          variants={item}
+        >
           <AiOutlineClockCircle className={styles.icon} />
           <p>{openingHours}</p>
         </Col>
--- src/features/businessInfo/components/AboutSection/AboutSection.js
+++ src/features/businessInfo/components/AboutSection/AboutSection.js
@@ -2,10 +2,38 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import LazyLoad from "react-lazy-load"
 import Container from "react-bootstrap/Container"
+import { motion } from "framer-motion"
 import SubSectionHeader from "../../../../shared/components/SubSectionHeader/SubSectionHeader"
 import { GatsbyImage } from "gatsby-plugin-image"
 import AboutCols from "./AboutCols/AboutCols"
 import * as styles from "./aboutSection.module.css"
+
+
+//START--Framer motion variants--START
+const aboutUs = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: 0.7,
+    },
+  },
+}
+
+const aboutUsImg = {
+  hidden: { opacity: 0, scale: 0.8 },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1.6,
+      delay: 0.5,
+      ease: [0, 0.71, 0.2, 1.01],
+    },
+  },
+}
+//END--Framer motion variants--END
 
 const About = () => {
   const data = useStaticQuery(graphql`
@@ -38,15 +66,28 @@ const About = () => {
     <LazyLoad offset={100}>
       <Container fluid="xl">
         <div className={styles.content}>
-          <div className={styles.aboutInfo}>
+          <motion.div
+            className={styles.aboutInfo}
+            variants={aboutUs}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ fallback: true, once: true }}
+          >
             <SubSectionHeader
               title="Sobre nosotros"
               className={styles.aboutContainer}
             >
               {sectionDesc}
             </SubSectionHeader>
-          </div>
-          <GatsbyImage image={aboutImage} alt="local" />
+          </motion.div>
+          <motion.div
+            variants={aboutUsImg}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ fallback: true, once: true }}
+          >
+            <GatsbyImage image={aboutImage} alt="local" />
+          </motion.div>
         </div>
         <AboutCols colsData={aboutColsData} />
       </Container>
