4d226b7cb7c89337c9baa362600f6081a21bca93
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-11-16 14:06:07 -0600

    Feat: Add icon to HeroCaption at Menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 import Seo from "../shared/components/Seo/seo"
 import HeroContainer from "../shared/components/HeroContainer/HeroContainer"
 import HeroCaption from "../shared/components/HeroContainer/HeroCaption/HeroCaption"
+import { MdOutlineFoodBank } from "react-icons/md";
 import Menu from "../features/menu/Menu/Menu"
 import Divider from "../shared/components/Divider/Divider"
 
@@ -20,6 +21,7 @@ const MenuPage = ({ data }) => {
           title={seoData.title}
           text={seoData.description}
           btnText="Explorar"
+          icon={<MdOutlineFoodBank/>}
         />
       </HeroContainer>
       <Divider />
