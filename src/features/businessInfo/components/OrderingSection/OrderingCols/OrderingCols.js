12a5e2e74b855dd847923708276c26c87286097f
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-11-15 19:46:11 -0600

    Feat: Home ordering info section animations

--- src/features/businessInfo/components/OrderingSection/OrderingCols/OrderingCols.js
+++ src/features/businessInfo/components/OrderingSection/OrderingCols/OrderingCols.js
@@ -1,32 +1,51 @@
 import React from "react"
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import { FaClipboardList, FaMotorcycle } from "react-icons/fa";
-import { BsFillChatDotsFill } from "react-icons/bs";
+import { Row, Col } from "react-bootstrap"
+import { motion } from "framer-motion"
+import { FaClipboardList, FaMotorcycle } from "react-icons/fa"
+import { BsFillChatDotsFill } from "react-icons/bs"
 import * as styles from "../OrderingCols/orderingCols.module.css"
+
+//START--Framer motion variants--START
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 1,
+      staggerChildren: 0.5,
+    },
+  },
+}
+
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1, transition: { duration: 1 } },
+}
+//END--Framer motion variants--END
 
 const OrderingCols = ({ colData }) => {
   return (
     <Container className={styles.mainCont}>
-      <Row className="justify-content-center">
-        <Col  lg={true} className={styles.col}>
-          <FaClipboardList className={styles.icon}/>
-          <p>
-            {colData.onlineOrdering}
-          </p>
+      <Row
+        as={motion.div}
+        className="justify-content-center"
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ fallback: true, once: true }}
+      >
+        <Col as={motion.div} lg={true} className={styles.col} variants={item}>
+          <FaClipboardList className={styles.icon} />
+          <p>{colData.onlineOrdering}</p>
         </Col>
-        <Col lg={true} className={styles.col}>
-          <FaMotorcycle className={styles.icon}/>
-          <p>
-            {colData.delivery}
-          </p>
+        <Col as={motion.div} lg={true} className={styles.col} variants={item}>
+          <FaMotorcycle className={styles.icon} />
+          <p>{colData.delivery}</p>
         </Col>
-        <Col lg={true} className={styles.col}>
-          <BsFillChatDotsFill className={styles.icon}/>
-          <p>
-            {colData.chatHelp}
-          </p>
+        <Col as={motion.div} lg={true} className={styles.col} variants={item}>
+          <BsFillChatDotsFill className={styles.icon} />
+          <p>{colData.chatHelp}</p>
         </Col>
       </Row>
     </Container>
@@ -34,7 +53,7 @@ const OrderingCols = ({ colData }) => {
 }
 
 OrderingCols.defaultProps = {
-  colData: {}
+  colData: {},
 }
 
 export default OrderingCols
--- src/features/businessInfo/components/OrderingSection/OrderingSection.js
+++ src/features/businessInfo/components/OrderingSection/OrderingSection.js
@@ -5,6 +5,20 @@ import Container from "react-bootstrap/Container"
 import SubSectionHeader from "../../../../shared/components/SubSectionHeader/SubSectionHeader"
 import OrderingCols from "./OrderingCols/OrderingCols"
 
+//START--Framer motion variants--START
+const header = {
+  hidden: { opacity: 0, x: -50 },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1.2,
+      delay: 0.5,
+    },
+  },
+}
+//END--Framer motion variants--END
+
 const OrderingSection = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -32,6 +46,10 @@ const OrderingSection = () => {
           withLink
           link="/menu"
           linkText="Ordenar"
+          variants={header}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ fallback: true, once: true, amount: 0.5 }}
         >
           {sectionDesc}
         </SubSectionHeader>
