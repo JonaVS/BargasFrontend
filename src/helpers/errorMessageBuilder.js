b47fb61ca24c7231672d9be275c9f59971a5b381
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-11-17 14:42:29 -0600

    Refactor: Logic changes for API calls error messages

--- src/helpers/errorMessageBuilder.js
+++ src/helpers/errorMessageBuilder.js
@@ -12,20 +12,14 @@ export const ErrorContext = {
 }
 
 export const errorMessageBuilder = (context, error) => {
-  let message = ""
-  if (isNetworkOrServerRelated(error))
-    return (message = "No se pudo conectar con el servidor")
+  if (error.response.status === 0)
+    return "No se pudo conectar con el servidor"
 
   const strapiError = error.response.data.error.message
 
-  message = StrapiErrors[context].find(
+  const message = StrapiErrors[context].find(
     err => err.strapiError === strapiError
-  ).translatedMessage
+  )
 
-  return message
-}
-
-const isNetworkOrServerRelated = error => {
-  if (error.response.status === 0 ) return true
-  return false
+  return message ? message.translatedMessage : 'Se ha producido un error'
 }
