5b61a23a73b9ab52093894a829d02c23f0a724b5
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-11-18 12:11:58 -0600

    Feat: UserAccount module section animation

--- src/features/userAccount/components/UserAccountModule/UserAccountModule.js
+++ src/features/userAccount/components/UserAccountModule/UserAccountModule.js
@@ -1,8 +1,29 @@
 import React from "react"
+import { motion } from "framer-motion"
 import * as styles from "./userAccountModule.module.css"
 
+const module = {
+  hidden: { opacity: 0, x: -50 },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1.2,
+    },
+  },
+}
+
 const UserAccountModule = ({ children }) => {
-  return <div className={styles.accountModule}>{children}</div>
+  return (
+    <motion.div
+      variants={module}
+      initial="hidden"
+      animate="show"
+      className={styles.accountModule}
+    >
+      {children}
+    </motion.div>
+  )
 }
 
 export default UserAccountModule
