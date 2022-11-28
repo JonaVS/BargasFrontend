1fe6262893fe1b053a2202a32182f01c9bd16dc3
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-11-28 16:42:27 -0600

    Refactor: Change Google Map custom pin

--- src/shared/components/GoogleMap/LocationPin/LocationPin.js
+++ src/shared/components/GoogleMap/LocationPin/LocationPin.js
@@ -1,7 +1,34 @@
 import React from "react"
-import { ImLocation2 } from "react-icons/im"
+import { motion } from "framer-motion"
+import { StaticImage } from "gatsby-plugin-image"
 import * as styles from "./locationPin.module.css"
 
-const LocationPin = () => <ImLocation2 className={styles.pinIcon} />
+//START--Framer motion variants--START
+const pin = {
+  show: {
+    scale: [1, 0.90],
+    transition: {
+      duration: 1.1,
+      repeat: Infinity,
+      repeatType: "reverse",
+      type: 'spring'
+    },
+  },
+}
+//END--Framer motion variants--END
+
+const LocationPin = () => (
+  <div className={styles.pinWrapper}>
+    <motion.div variants={pin} animate="show" className={styles.imgHolder}>
+      <StaticImage
+        src="../../../../images/logoB.png"
+        alt="business pin"
+        placeholder="blurred"
+        width={40}
+        quality={100}
+      />
+    </motion.div>
+  </div>
+)
 
 export default LocationPin
