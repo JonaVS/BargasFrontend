3546dad95a0e64340ac8893b7d3700c90698744f
JonaVS <79957968+JonaVS@users.noreply.github.com>
2022-11-30 19:32:46 -0600

    Refactor: Remove unnecessary fragment + minor changes at EditProductModal

--- src/features/cart/components/EditProductModal/EditProductModal.js
+++ src/features/cart/components/EditProductModal/EditProductModal.js
@@ -1,41 +1,38 @@
 import React from "react"
-import Modal from "react-bootstrap/Modal"
-import Badge from "react-bootstrap/Badge"
+import {Modal, Badge} from "react-bootstrap"
 import { GatsbyImage } from "gatsby-plugin-image"
-import *as styles from "./editProductModal.module.css"
 import ProductForm from "../../../../shared/components/Form/ProductForm/ProductForm"
+import *as styles from "./editProductModal.module.css"
 
-const EditProductModal = ({ showModal, onHide, product, currentSort }) => {
+const EditProductModal = ({ showModal, onHide, product }) => {
   return (
-    <>
-      <Modal
-        contentClassName="p-0 border-0"
-        centered
-        show={showModal}
-        onHide={onHide}
-      >
-        <Modal.Header className={styles.modalHeader} closeButton>
-          <h4 className={styles.modalTittle}>
-            <span className={styles.productName}>{product.name}</span>
-            <Badge bg="success" className="ms-2 fw-normal">
-              ₡ {new Intl.NumberFormat("CRC").format(product.price)}
-            </Badge>
-          </h4>
-        </Modal.Header>
-        <Modal.Body className={styles.modalBody}>
-          {product.qualityImg && (
-            <GatsbyImage alt="Product image" image={product.qualityImg} />
-          )}
-          <div className={styles.formWrapper}>
-            <ProductForm
-              productData={product}
-              isEditMode
-              handleCloseEditModal={onHide}
-            />
-          </div>
-        </Modal.Body>
-      </Modal>
-    </>
+    <Modal
+      contentClassName={styles.modal}
+      centered
+      show={showModal}
+      onHide={onHide}
+    >
+      <Modal.Header className={styles.modalHeader} closeButton>
+        <h4 className={styles.modalTittle}>
+          <span className={styles.productName}>{product.name}</span>
+          <Badge bg="success" className="ms-2 fw-normal">
+            ₡ {new Intl.NumberFormat("CRC").format(product.price)}
+          </Badge>
+        </h4>
+      </Modal.Header>
+      <Modal.Body className={styles.modalBody}>
+        {product.qualityImg && (
+          <GatsbyImage alt="Product image" image={product.qualityImg} />
+        )}
+        <div className={styles.formWrapper}>
+          <ProductForm
+            productData={product}
+            isEditMode
+            handleCloseEditModal={onHide}
+          />
+        </div>
+      </Modal.Body>
+    </Modal>
   )
 }
 
